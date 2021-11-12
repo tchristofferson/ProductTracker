55c408caf6e609959c5b4519f4b7e30bb0191c8b
tchristofferson <longleadtrc@gmail.com>
2021-11-12 15:15:42 -0600

    Convert App component to class component to hold state

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -5,36 +5,49 @@ import Properties from "./components/Properties";
 import PropertyLocations from "./components/PropertyLocations";
 import { BrowserRouter, Route } from "react-router-dom";
 
-const App = () => {
-  useEffect(() => {
-    axios
-      .get("http://localhost:8080/properties", {
-        //Adding this automatically adds header
-        auth: {
-          username: 'admin',
-          password: 'password'
-        }
-      })
-      .then(function (response) {
-        console.log(response);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  });
+class App extends React.Component {
 
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Navigation />
-        <div className="container">
-          <Route exact path="/" component={Properties} />
-          <Route path="/properties" component={Properties} />
-          <Route path="propertyLocations" component={PropertyLocations} />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-};
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      properties: [],
+      propertyLocations: [],
+      products: []
+    }
+  }
+
+  render() {
+    useEffect(() => {
+      axios
+          .get("http://localhost:8080/properties", {
+            //Adding this automatically adds header
+            auth: {
+              username: 'admin',
+              password: 'password'
+            }
+          })
+          .then(function (response) {
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+    });
+
+    return (
+        <BrowserRouter>
+          <div className="App">
+            <Navigation/>
+            <div className="container">
+              <Route exact path="/" component={Properties}/>
+              <Route path="/properties" component={Properties}/>
+              <Route path="propertyLocations" component={PropertyLocations}/>
+            </div>
+          </div>
+        </BrowserRouter>
+    );
+  }
+}
 
 export default App;
